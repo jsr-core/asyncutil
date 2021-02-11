dcd3aa9ab2e2905a19239c15e1a4d06e0f90f2f8
Alisue <lambdalisue@hashnote.net>
2021-02-11 15:16:03 +0900

    Add Usage and example codes

---
+++ examples/event.ts
@@ -0,0 +1,28 @@
+import { Event } from "../mod.ts";
+import { delay } from "https://deno.land/std@0.86.0/async/mod.ts";
+
+const event = new Event();
+
+const task1 = async () => {
+  await event.wait();
+  console.log("Task1 complete");
+};
+
+const task2 = async () => {
+  await event.wait();
+  console.log("Task2 complete");
+};
+
+const task3 = async () => {
+  await delay(100);
+  console.log("Hello");
+  event.set();
+  await delay(100);
+  console.log("World");
+};
+
+await Promise.all([task1(), task2(), task3()]);
+// Hello
+// Task1 complete
+// Task2 complete
+// World
