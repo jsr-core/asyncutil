55e3523c0231b215230cab7bbd1416dd65ff9c36
Alisue <lambdalisue@hashnote.net>
2021-04-12 00:55:45 +0900

    Update dependenceis

--- deps_test.ts
+++ deps_test.ts
@@ -1,6 +1,6 @@
 export {
   assertEquals,
   assertThrows,
-} from "https://deno.land/std@0.86.0/testing/asserts.ts";
+} from "https://deno.land/std@0.92.0/testing/asserts.ts";
 
-export { delay } from "https://deno.land/std@0.86.0/async/mod.ts";
+export { delay } from "https://deno.land/std@0.92.0/async/mod.ts";
