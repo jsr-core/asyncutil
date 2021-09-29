55ece7631f943e016a29409ab7549f0f5046e090
GitHub <noreply@github.com>
2021-09-29 01:04:21 +0000

    :package: Update deno dependencies

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
-export { deferred } from "https://deno.land/std@0.108.0/async/deferred.ts";
+export { deferred } from "https://deno.land/std@0.109.0/async/deferred.ts";
 
-export type { Deferred } from "https://deno.land/std@0.108.0/async/deferred.ts";
+export type { Deferred } from "https://deno.land/std@0.109.0/async/deferred.ts";
