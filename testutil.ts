170f4bfd6df6df12fdfe3faaef88320c574257f8
Alisue <lambdalisue@gmail.com>
2023-05-05 20:18:15 +0900

    Add `AsyncValue` class to `testutil.ts`

---
+++ testutil.ts
@@ -0,0 +1,44 @@
+/**
+ * A class that wraps a value and allows it to be set asynchronously.
+ *
+ * ```ts
+ * import { assertEquals } from "https://deno.land/std@0.186.0/testing/asserts.ts";
+ * import { AsyncValue } from "./testutil.ts";
+ *
+ * const v = new AsyncValue(0);
+ * assertEquals(await v.get(), 0);
+ * await v.set(1);
+ * assertEquals(await v.get(), 1);
+ * ```
+ *
+ * @typeParam T - The type of the value.
+ */
+export class AsyncValue<T> {
+  #value: T;
+
+  /**
+   * Constructs a new AsyncValue with the given initial value.
+   *
+   * @param value - The initial value.
+   */
+  constructor(value: T) {
+    this.#value = value;
+  }
+
+  /**
+   * Returns the current value.
+   */
+  get(): Promise<T> {
+    return new Promise((resolve) => resolve(this.#value));
+  }
+
+  /**
+   * Sets the value.
+   */
+  set(value: T): Promise<void> {
+    return new Promise((resolve) => {
+      this.#value = value;
+      resolve();
+    });
+  }
+}
---
+++ testutil_test.ts
@@ -0,0 +1,14 @@
+import { assertEquals } from "https://deno.land/std@0.186.0/testing/asserts.ts";
+import { AsyncValue } from "./testutil.ts";
+
+Deno.test("AsyncValue", async (t) => {
+  await t.step(
+    "'get' returns a promise that resolves to the value set by 'set'",
+    async () => {
+      const v = new AsyncValue(0);
+      assertEquals(await v.get(), 0);
+      await v.set(1);
+      assertEquals(await v.get(), 1);
+    },
+  );
+});
