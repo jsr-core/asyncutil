158cf2d6f16cc1f9d1aea0c7176d8977cd5b02dc
Alisue <lambdalisue@gmail.com>
2023-05-05 21:16:53 +0900

    Add `Stack` to `stack.ts`

---
+++ stack.ts
@@ -0,0 +1,68 @@
+import { Notify, WaitOptions } from "./notify.ts";
+
+/**
+ * A stack implementation that allows for adding and removing elements, with optional waiting when
+ * popping elements from an empty stack.
+ *
+ * ```ts
+ * import { assertEquals } from "https://deno.land/std@0.186.0/testing/asserts.ts";
+ * import { Stack } from "./stack.ts";
+ *
+ * const stack = new Stack<number>();
+ * stack.push(1);
+ * stack.push(2);
+ * stack.push(3);
+ * assertEquals(await stack.pop(), 3);
+ * assertEquals(await stack.pop(), 2);
+ * assertEquals(await stack.pop(), 1);
+ * ```
+ *
+ * @template T The type of items in the stack.
+ */
+export class Stack<T> {
+  #notify = new Notify();
+  #items: T[] = [];
+
+  /**
+   * Gets the number of items in the queue.
+   */
+  get size(): number {
+    return this.#items.length;
+  }
+
+  /**
+   * Returns true if the stack is currently locked.
+   */
+  get locked(): boolean {
+    return this.#notify.waiters > 0;
+  }
+
+  /**
+   * Adds an item to the top of the stack and notifies any waiting consumers.
+   *
+   * @param {T} value The item to add to the stack.
+   */
+  push(value: T): void {
+    this.#items.push(value);
+    this.#notify.notify();
+  }
+
+  /**
+   * Removes the next item from the stack, optionally waiting if the stack is currently empty.
+   *
+   * @param {WaitOptions} [options] Optional parameters to pass to the wait operation.
+   * @param {AbortSignal} [options.signal] An optional AbortSignal used to abort the wait operation if the signal is aborted.
+   * @returns {Promise<T>} A promise that resolves to the next item in the stack.
+   * @throws {DOMException} Throws a DOMException with "Aborted" and "AbortError" codes if the wait operation was aborted.
+   */
+  async pop({ signal }: WaitOptions = {}): Promise<T> {
+    while (!signal?.aborted) {
+      const value = this.#items.pop();
+      if (value) {
+        return value;
+      }
+      await this.#notify.notified({ signal });
+    }
+    throw new DOMException("Aborted", "AbortError");
+  }
+}
---
+++ stack_test.ts
@@ -0,0 +1,61 @@
+import { delay } from "https://deno.land/std@0.186.0/async/delay.ts";
+import {
+  assertEquals,
+  assertRejects,
+} from "https://deno.land/std@0.186.0/testing/asserts.ts";
+import { promiseState } from "./state.ts";
+import { Stack } from "./stack.ts";
+
+Deno.test("Stack", async (t) => {
+  await t.step("'pop' returns pushed items", async () => {
+    const q = new Stack<number>();
+    q.push(1);
+    q.push(2);
+    q.push(3);
+    assertEquals(await q.pop(), 3);
+    assertEquals(await q.pop(), 2);
+    assertEquals(await q.pop(), 1);
+  });
+
+  await t.step("'pop' waits for an item is pushed", async () => {
+    const q = new Stack<number>();
+    const popper = q.pop();
+    assertEquals(await promiseState(popper), "pending");
+    q.push(1);
+    assertEquals(await promiseState(popper), "fulfilled");
+    assertEquals(await popper, 1);
+  });
+
+  await t.step("'pop' with non-aborted signal", async () => {
+    const controller = new AbortController();
+    const q = new Stack<number>();
+    const popper = q.pop({ signal: controller.signal });
+    assertEquals(await promiseState(popper), "pending");
+  });
+
+  await t.step("'pop' with signal aborted after delay", async () => {
+    const controller = new AbortController();
+    const q = new Stack<number>();
+
+    delay(100).then(() => controller.abort());
+
+    await assertRejects(
+      () => q.pop({ signal: controller.signal }),
+      DOMException,
+      "Aborted",
+    );
+  });
+
+  await t.step("'pop' with signal already aborted", async () => {
+    const controller = new AbortController();
+    const q = new Stack<number>();
+
+    controller.abort();
+
+    await assertRejects(
+      () => q.pop({ signal: controller.signal }),
+      DOMException,
+      "Aborted",
+    );
+  });
+});
