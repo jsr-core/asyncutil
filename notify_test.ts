aa451d086d38cc8f16e806f2386db2f5d246550a
Alisue <lambdalisue@gmail.com>
2024-08-13 15:43:32 +0900

    test(notify): improve `Notify` tests

--- notify_test.ts
+++ notify_test.ts
@@ -8,21 +8,61 @@ Deno.test("Notify", async (t) => {
     const notify = new Notify();
     const waiter1 = notify.notified();
     const waiter2 = notify.notified();
+    assertEquals(notify.waiterCount, 2);
 
     notify.notify();
+    assertEquals(notify.waiterCount, 1);
     assertEquals(await promiseState(waiter1), "fulfilled");
     assertEquals(await promiseState(waiter2), "pending");
 
     notify.notify();
+    assertEquals(notify.waiterCount, 0);
     assertEquals(await promiseState(waiter1), "fulfilled");
     assertEquals(await promiseState(waiter2), "fulfilled");
+  });
+
+  await t.step("'notify' wakes up a multiple waiters", async () => {
+    const notify = new Notify();
+    const waiter1 = notify.notified();
+    const waiter2 = notify.notified();
+    const waiter3 = notify.notified();
+    const waiter4 = notify.notified();
+    const waiter5 = notify.notified();
+    assertEquals(notify.waiterCount, 5);
+
+    notify.notify(2);
+    assertEquals(notify.waiterCount, 3);
+    assertEquals(await promiseState(waiter1), "fulfilled");
+    assertEquals(await promiseState(waiter2), "fulfilled");
+    assertEquals(await promiseState(waiter3), "pending");
+    assertEquals(await promiseState(waiter4), "pending");
+    assertEquals(await promiseState(waiter5), "pending");
+
+    notify.notify(2);
+    assertEquals(notify.waiterCount, 1);
+    assertEquals(await promiseState(waiter1), "fulfilled");
+    assertEquals(await promiseState(waiter2), "fulfilled");
+    assertEquals(await promiseState(waiter3), "fulfilled");
+    assertEquals(await promiseState(waiter4), "fulfilled");
+    assertEquals(await promiseState(waiter5), "pending");
+
+    notify.notify(2);
+    assertEquals(notify.waiterCount, 0);
+    assertEquals(await promiseState(waiter1), "fulfilled");
+    assertEquals(await promiseState(waiter2), "fulfilled");
+    assertEquals(await promiseState(waiter3), "fulfilled");
+    assertEquals(await promiseState(waiter4), "fulfilled");
+    assertEquals(await promiseState(waiter5), "fulfilled");
   });
 
   await t.step("'notifyAll' wakes up all waiters", async () => {
     const notify = new Notify();
     const waiter1 = notify.notified();
     const waiter2 = notify.notified();
+    assertEquals(notify.waiterCount, 2);
+
     notify.notifyAll();
+    assertEquals(notify.waiterCount, 0);
     assertEquals(await promiseState(waiter1), "fulfilled");
     assertEquals(await promiseState(waiter2), "fulfilled");
   });
